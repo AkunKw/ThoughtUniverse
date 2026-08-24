8edbec1ae041af805ecc99195eb320fc9fb8e633
AkunKw <akw03097@gmail.com>
2026-08-24 03:06:15 +0300

    feat: add remaining Explore constellations

--- src/components/explore/ConstellationField.tsx
+++ src/components/explore/ConstellationField.tsx
@@ -82,6 +82,67 @@ const constellations: Constellation[] = [
       { from: "2-7", to: "2-8" },
     ],
   },
+  {
+    id: "constellation-3",
+    stars: [
+      { id: "3-1", x: 46.9, y: 74.6, size: 34 },
+      { id: "3-2", x: 43.9, y: 84.3, size: 28 },
+      { id: "3-3", x: 56.7, y: 82.4, size: 32 },
+      { id: "3-4", x: 64, y: 94.6, size: 28 },
+      { id: "3-5", x: 53.7, y: 92.9, size: 30 },
+      { id: "3-6", x: 48.2, y: 99.8, size: 32 },
+    ],
+    connections: [
+      { from: "3-1", to: "3-2" },
+      { from: "3-2", to: "3-3" },
+      { from: "3-3", to: "3-4" },
+      { from: "3-4", to: "3-5" },
+      { from: "3-5", to: "3-6" },
+    ],
+  },
+  {
+    id: "constellation-4",
+    stars: [
+      { id: "4-1", x: 28.5, y: 67, size: 32 },
+      { id: "4-2", x: 36.5, y: 69.5, size: 28 },
+      { id: "4-3", x: 20.4, y: 71.8, size: 30 },
+      { id: "4-4", x: 25.8, y: 79.7, size: 26 },
+      { id: "4-5", x: 33.8, y: 77.7, size: 32 },
+      { id: "4-6", x: 39, y: 92.9, size: 28 },
+      { id: "4-7", x: 31.1, y: 87.8, size: 30 },
+      { id: "4-8", x: 21.9, y: 93.5, size: 28 },
+    ],
+    connections: [
+      { from: "4-1", to: "4-2" },
+      { from: "4-1", to: "4-3" },
+      { from: "4-3", to: "4-4" },
+      { from: "4-4", to: "4-5" },
+      { from: "4-5", to: "4-6" },
+      { from: "4-6", to: "4-7" },
+      { from: "4-7", to: "4-8" },
+    ],
+  },
+  {
+    id: "constellation-5",
+    stars: [
+      { id: "5-1", x: 64, y: 68.9, size: 28 },
+      { id: "5-2", x: 74, y: 64.3, size: 32 },
+      { id: "5-3", x: 69, y: 81.3, size: 28 },
+      { id: "5-4", x: 74.8, y: 90, size: 32 },
+      { id: "5-5", x: 79.2, y: 77.4, size: 34 },
+      { id: "5-6", x: 89.7, y: 72.1, size: 30 },
+      { id: "5-7", x: 93.1, y: 89.2, size: 32 },
+    ],
+    connections: [
+      { from: "5-1", to: "5-2" },
+      { from: "5-2", to: "5-3" },
+      { from: "5-3", to: "5-4" },
+      { from: "5-4", to: "5-5" },
+      { from: "5-5", to: "5-6" },
+      { from: "5-6", to: "5-7" },
+    ],
+  },
+
 ];
 
 export function ConstellationField() {
